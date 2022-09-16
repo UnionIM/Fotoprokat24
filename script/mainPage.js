ccca969ac5fb654aff8a1c588f670d1781ec0033
UnionIM <Dan.rowv@Gmail.com>
2022-09-16 17:38:05 +0300

    Maun page

---
+++ script/mainPage.js
@@ -0,0 +1,38 @@
+const infoLink = document.querySelectorAll('.header__link[data-goto]')
+
+const scrollTo = (e, flag = true) => {
+  let infoLink
+  if(flag){
+    infoLink = e.target;
+  }else{
+    infoLink = e;
+  }
+
+  if(infoLink.dataset.goto && document.querySelector(infoLink.dataset.goto)){
+    const gotoBlock = document.querySelector(infoLink.dataset.goto);
+    const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+    window.scrollTo({
+      top: gotoBlockValue,
+      behavior: "smooth"
+    })
+    if(flag){
+      e.preventDefault();
+    }
+  }
+}
+
+if(infoLink.length > 0){
+  infoLink.forEach(el => {
+    el.addEventListener('click', scrollTo)
+  })
+}
+
+infoLink.forEach((el, ind) => {
+  if(window.sessionStorage.getItem('0') === ind.toString()){
+    console.log(ind.toString())
+    scrollTo(el, false)
+  }
+})
+
+console.log(window.sessionStorage)
